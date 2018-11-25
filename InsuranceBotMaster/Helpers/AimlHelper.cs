796af4eae06fb473949862775c90657f12b22328
KRUROL <rolands.krumbergs@if.lv>
2018-11-25 23:21:03 +0200

    Capitalize first letter for AIML result.

--- InsuranceBotMaster/Helpers/AimlHelper.cs
+++ InsuranceBotMaster/Helpers/AimlHelper.cs
@@ -41,6 +41,9 @@ namespace InsuranceBotMaster.Helpers
             logger.LogAimlResult(utteranceInEnglish, output.RawOutput);
 
             var resultInNorwegian = await translator.TranslateFromEnglishToNorwegian(output.RawOutput);
+
+            resultInNorwegian = FirstLetterToUpper(resultInNorwegian);
+
             logger.LogTranslationResult(output.RawOutput, resultInNorwegian, "English", "Norwegian");
 
             return resultInNorwegian;
@@ -69,6 +72,17 @@ namespace InsuranceBotMaster.Helpers
             logger.LogAimlResult(query, output.RawOutput);
 
             return output.RawOutput;
+        }
+
+        public static string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
         }
     }
 }
